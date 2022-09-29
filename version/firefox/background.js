8cb0edd4c4efff874c0d2c500feab6e4cc3bb14b
NopeCHA <jaewany@gmail.com>
2022-09-29 01:09:50 -0700

    WIP set language Firefox

--- version/firefox/background.js
+++ version/firefox/background.js
@@ -465,6 +465,18 @@
 
 
     (async () => {
+        const lang = 'en-US';
+        browser.webRequest.onBeforeSendHeaders.addListener(e => {
+            const url = new URL(e.url);
+            if (url.searchParams.get('hl') !== lang) {
+                url.searchParams.set('hl', lang);
+                return {redirectUrl: url.toString()};
+            }
+        }, {urls: [
+            '*://*.google.com/recaptcha/*',
+            '*://*.recaptcha.net/recaptcha/*',
+        ], types: ['sub_frame']}, ['blocking']);
+
         // await Server.check_status();
         // Server.run_status_check();
 
