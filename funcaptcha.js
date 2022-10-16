6a19169a5bd5c97e1918023964e182ca504412cf
Jaewan Yun <jay50@pitt.edu>
2022-10-16 10:27:15 -0700

    FunCAPTCHA dataset (#12)
    * add ocr captcha (incomplete, proof-of-concept)
    
    * add ocr captcha (incomplete, proof-of-concept)
    
    * add ocr selectors (proof-of-concept)
    
    * api get response, add unicode conversion
    
    * scrape funcaptcha

---
+++ funcaptcha.js
@@ -0,0 +1,70 @@
+(async () => {
+    class BG {
+        static exec(method, data) {
+            return new Promise(resolve => {
+                try {
+                    chrome.runtime.sendMessage({method, data}, resolve);
+                } catch (e) {
+                    console.log('exec failed', e);
+                    resolve();
+                }
+            });
+        }
+    }
+
+    class Net {
+        static async fetch(url, options) {
+            return await BG.exec('fetch', {url, options});
+        }
+    }
+
+
+    function sleep(t) {
+        return new Promise(resolve => setTimeout(resolve, t));
+    }
+
+
+    function get_task() {
+        const $task = document.querySelector('#game_children_text > h2');
+        return $task?.innerText;
+    }
+
+
+    function get_image() {
+        const $image = document.querySelector('img#game_challengeItem_image');
+        return $image?.src;
+    }
+
+
+    async function on_click(index) {
+        const task = get_task();
+        const image = get_image();
+        // console.log('clicked', index, task);
+        if (task && image) {
+            const data = {task, image, index};
+            // console.log('sending', data);
+            await Net.fetch('https://api.nopecha.com/upload', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(data),
+            });
+        }
+    }
+
+
+    while (true) {
+        await sleep(1000);
+
+        const $verify = document.querySelector('#home_children_button');
+        if ($verify) {
+            $verify.click();
+        }
+
+        const $btns = document.querySelectorAll('#game_children_challenge ul > li > a');
+        for (const i in $btns) {
+            const $e = $btns[i];
+            $e.removeEventListener('click', on_click.bind(this, parseInt(i)));
+            $e.addEventListener('click', on_click.bind(this, parseInt(i)));
+        }
+    }
+})();
