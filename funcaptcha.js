0da2242b3091e820613a872fb8458bc6e1f392f0
Jaewan Yun <jay50@pitt.edu>
2022-10-16 11:03:38 -0700

    fix scraping after first challenge

--- funcaptcha.js
+++ funcaptcha.js
@@ -39,10 +39,10 @@
     async function on_click(index) {
         const task = get_task();
         const image = get_image();
-        // console.log('clicked', index, task);
+        console.log('clicked', index);
         if (task && image) {
             const data = {task, image, index};
-            // console.log('sending', data);
+            console.log('uploading', data);
             await Net.fetch('https://api.nopecha.com/upload', {
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
@@ -55,16 +55,21 @@
     while (true) {
         await sleep(1000);
 
-        const $verify = document.querySelector('#home_children_button');
-        if ($verify) {
-            $verify.click();
-        }
+        try {
+            const $verify = document.querySelector('#home_children_button');
+            if ($verify) {
+                $verify.click();
+            }
 
-        const $btns = document.querySelectorAll('#game_children_challenge ul > li > a');
-        for (const i in $btns) {
-            const $e = $btns[i];
-            $e.removeEventListener('click', on_click.bind(this, parseInt(i)));
-            $e.addEventListener('click', on_click.bind(this, parseInt(i)));
+            const $btns = document.querySelectorAll('#game_children_challenge ul > li > a');
+            console.log('$btns', $btns);
+            for (const i in $btns) {
+                const $e = $btns[i];
+                $e.removeEventListener('click', on_click);
+                $e.addEventListener('click', on_click.bind(this, parseInt(i)));
+            }
+        } catch (e) {
+            // console.log('error', e);
         }
     }
 })();
