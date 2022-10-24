28af43f5ecf024fd7dd18addad6f4374d592aeef
Jaewan Yun <jay50@pitt.edu>
2022-10-24 11:43:15 -0700

    fix https://nopecha.com/setup

--- setup.js
+++ setup.js
@@ -1,23 +1,11 @@
 const url = new URL(document.location);
 
-class BG {
-    static exec(method, data) {
-        return new Promise(resolve => {
-            try {
-                chrome.runtime.sendMessage({method, data}, resolve);
-            } catch (e) {
-                console.log('exec failed', e);
-                resolve();
-            }
-        });
-    }
-}
 
-
-if(document.location.hash) {
+if (document.location.hash) {
     document.body.innerText = "Loading...";
     BG.exec("set_settings", { id: "key", value: document.location.hash.substring(1) })
         .then(() => document.body.innerText = "Key set!");
-} else {
+}
+else {
     document.body.innerText = "Missing key. Please set the hash and reload the page.\nExample: https://nopecha.com/setup#sub_testkey1234";
 }
